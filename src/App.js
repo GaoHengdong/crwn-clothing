66b58e6114dea410d5338f4c47444b3a6585c9c2
Gao Hengdong <hengdonggao@outlook.com>
2020-04-01 14:55:14 +0800

    add auth

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import Homepage from "./pages/homepage/Homepage.js";
 import Shop from "./pages/shop/Shop";
 import Header from "./components/header/Header";
 import SignIn from "./pages/signin/SignIn";
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" render={() => <Homepage />} />
-        <Route exact path="/shop" render={() => <Shop />} />
-        <Route exact path="/signin" render={() => <SignIn />} />
-      </Switch>
-    </div>
-  );
+import { auth } from "./firebase/firebase.util";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { user: "" };
+  }
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      console.log(user);
+      this.setState({ user: user });
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" render={() => <Homepage />} />
+          <Route exact path="/shop" render={() => <Shop />} />
+          <Route exact path="/signin" render={() => <SignIn />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
