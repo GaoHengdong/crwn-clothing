2b0d8515771fd1cdba08861fef6f6b2691c01e10
Gao Hengdong <hengdonggao@outlook.com>
2020-07-28 14:24:53 +0800

    add hooks to sign up

--- src/components/siginin-form/SignInForm.js
+++ src/components/siginin-form/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import FormInput from "../form-input/FormInput";
 import CustomButton from "../custom-button/CustomButton";
 import { connect } from "react-redux";
@@ -8,65 +8,56 @@ import {
 } from "../../redux/user/user.action";
 import "./SignInForm.scss";
 
-class SignInForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-    };
-    this.handleFormChange = this.handleFormChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+const SignInForm = ({ emailSignInStart, googleSignInStart }) => {
+  const [userCredentials, setUserCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  function handleFormChange(e) {
+    setUserCredentials({ ...userCredentials, [e.target.name]: e.target.value });
   }
-  handleFormChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-  async handleFormSubmit(e) {
+  async function handleFormSubmit(e) {
     e.preventDefault();
-    const { email, password } = this.state;
-    const { emailSignInStart } = this.props;
+    const { email, password } = userCredentials;
     emailSignInStart({ email, password });
   }
-  render() {
-    const { googleSignInStart } = this.props;
-    return (
-      <div className="SignInForm">
-        <h2 className="SignInForm__title">已经有账号</h2>
-        <h3 className="SignInForm__subtitle">请用邮箱和密码登录</h3>
-        <form className="SignInForm__form" onSubmit={this.handleFormSubmit}>
-          <FormInput
-            type="email"
-            name="email"
-            id="SignInForm__email"
-            value={this.state.email}
-            onChange={this.handleFormChange}
-            label="电子邮件"
-            required
-          />
-          <FormInput
-            type="password"
-            name="password"
-            id="SignInForm__password"
-            value={this.state.password}
-            onChange={this.handleFormChange}
-            label="密码"
-            required
-          />
-          <div className="SignInForm__btn-container">
-            <CustomButton type="submit">登录</CustomButton>
-            <CustomButton
-              type="button"
-              className="google-btn"
-              onClick={googleSignInStart}
-            >
-              用google登录
-            </CustomButton>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="SignInForm">
+      <h2 className="SignInForm__title">已经有账号</h2>
+      <h3 className="SignInForm__subtitle">请用邮箱和密码登录</h3>
+      <form className="SignInForm__form" onSubmit={handleFormSubmit}>
+        <FormInput
+          type="email"
+          name="email"
+          id="SignInForm__email"
+          value={userCredentials.email}
+          onChange={handleFormChange}
+          label="电子邮件"
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          id="SignInForm__password"
+          value={userCredentials.password}
+          onChange={handleFormChange}
+          label="密码"
+          required
+        />
+        <div className="SignInForm__btn-container">
+          <CustomButton type="submit">登录</CustomButton>
+          <CustomButton
+            type="button"
+            className="google-btn"
+            onClick={googleSignInStart}
+          >
+            用google登录
+          </CustomButton>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   googleSignInStart: () => dispatch(googleSignInStart()),
--- src/components/signup-form/SignUpForm.js
+++ src/components/signup-form/SignUpForm.js
@@ -1,85 +1,79 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import FormInput from "../form-input/FormInput";
 import CustomButton from "../custom-button/CustomButton";
 import { signUpStart } from "../../redux/user/user.action";
 import { connect } from "react-redux";
 import "./SignUpForm.scss";
 
-class SignUpForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      email: "",
-      password: "",
-      confirm_password: "",
-    };
-    this.handleFormChange = this.handleFormChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
-  handleFormChange(e) {
-    this.setState({
+const SignUpForm = ({ signUpStart }) => {
+  const [userCredentials, setUserCredentials] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
+  function handleFormChange(e) {
+    setUserCredentials({
+      ...userCredentials,
       [e.target.name]: e.target.value,
     });
   }
-  async handleFormSubmit(e) {
+  async function handleFormSubmit(e) {
     e.preventDefault();
     //检查密码
-    const { password, confirm_password, username, email } = this.state;
-    const { signUpStart } = this.props;
+    const { password, confirm_password, username, email } = userCredentials;
     if (password !== confirm_password) {
       alert("您输入的密码不一致，请重新输入！");
     }
     signUpStart({ email, password, displayName: username });
   }
-  render() {
-    return (
-      <div className="SignUpForm">
-        <h2 className="SignUpForm__title">没有账号</h2>
-        <div className="SignUpForm__subtitle">请用邮箱和密码注册</div>
-        <form className="SignUpForm__form" onSubmit={this.handleFormSubmit}>
-          <FormInput
-            type="username"
-            name="username"
-            id="SignUpForm__username"
-            value={this.state.username}
-            onChange={this.handleFormChange}
-            label="用户名"
-            required
-          />
-          <FormInput
-            type="email"
-            name="email"
-            id="SignUpForm__email"
-            value={this.state.email}
-            onChange={this.handleFormChange}
-            label="电子邮件"
-            required
-          />
-          <FormInput
-            type="password"
-            name="password"
-            id="SignUpForm__password"
-            value={this.state.password}
-            onChange={this.handleFormChange}
-            label="密码"
-            required
-          />
-          <FormInput
-            type="password"
-            name="confirm_password"
-            id="SignUpForm__confirm_password"
-            value={this.state.confirm_password}
-            onChange={this.handleFormChange}
-            label="确认密码"
-            required
-          />
-          <CustomButton type="submit">注册</CustomButton>
-        </form>
-      </div>
-    );
-  }
-}
+
+  return (
+    <div className="SignUpForm">
+      <h2 className="SignUpForm__title">没有账号</h2>
+      <div className="SignUpForm__subtitle">请用邮箱和密码注册</div>
+      <form className="SignUpForm__form" onSubmit={handleFormSubmit}>
+        <FormInput
+          type="username"
+          name="username"
+          id="SignUpForm__username"
+          value={userCredentials.username}
+          onChange={handleFormChange}
+          label="用户名"
+          required
+        />
+        <FormInput
+          type="email"
+          name="email"
+          id="SignUpForm__email"
+          value={userCredentials.email}
+          onChange={handleFormChange}
+          label="电子邮件"
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          id="SignUpForm__password"
+          value={userCredentials.password}
+          onChange={handleFormChange}
+          label="密码"
+          required
+        />
+        <FormInput
+          type="password"
+          name="confirm_password"
+          id="SignUpForm__confirm_password"
+          value={userCredentials.confirm_password}
+          onChange={handleFormChange}
+          label="确认密码"
+          required
+        />
+        <CustomButton type="submit">注册</CustomButton>
+      </form>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   signUpStart: (userCredentials) => dispatch(signUpStart(userCredentials)),
